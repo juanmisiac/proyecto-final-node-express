d7e82c15def768665aab9b24066406674e8f840c
Juan <juanmisiac@gmail.com>
2026-06-27 01:28:17 -0300

    add: some products routes working perfectly. next: delete products

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -1,15 +1,56 @@
-export const getAllProducts = () => {
+import { getProductsService } from "../services/products.services.js";
+import { getProductsByIdService } from "../services/products.services.js";
+import { postProductService } from "../services/products.services.js";
 
+// Procesar solicitud -> Generar respuesta
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await getProductsService();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error interno del servidor.",
+      error: error.message
+    });
+  }
 }
 
-export const getProductsById = () => {
-
+export const getProductsById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const product = await getProductsByIdService(id);
+    if (!product) {
+      return res.status(404).json({ message: "No se encontro ningun producto con ese id." })
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error interno del servidor.",
+      error: error.message
+    });
+  }
 }
 
-export const postProduct = () => {
-
+export const postProduct = async (req, res) => {
+  const { category, name, team, price, stock} = req.body;
+  const newProduct = {
+    category,
+    name,
+    team,
+    price,
+    stock
+  }
+  try {
+    const response = await postProductService(newProduct);
+    res.status(200).json({ message: "Producto creado." });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error interno del servidor.",
+      error: error.message
+    });
+  }
 }
 
 export const deleteProduct = () => {
-  
+
 }
---
+++ src/services/products.services.js
@@ -0,0 +1,28 @@
+import * as productModel from "../models/product.model.js";
+
+export const getProductsService = async () => {
+  try {
+    const products = await productModel.getAllProducts();
+    return products;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const getProductsByIdService = async (id) => {
+  try {
+    const product = await productModel.getProductById(id);
+    return product;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const postProductService = async (product) => {
+  try {
+    const response = await productModel.saveProduct(product);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
